7d17f317a0d71e265e87e4006bb5093cd5235ea9
Rahul Pramod Jaguste <rahuljaguste@gmail.com>
2022-05-03 00:42:50 +0530

    1st assignment

--- Q2/contracts/circuits/Multiplier3.circom
+++ Q2/contracts/circuits/Multiplier3.circom
@@ -2,16 +2,35 @@ pragma circom 2.0.0;
 
 // [assignment] Modify the circuit below to perform a multiplication of three signals
 
+
+template Multiplier2 () {  
+
+   // Declaration of signals.  
+   signal input a;  
+   signal input b;  
+   signal output c;  
+
+   // Constraints.  
+   c <== a * b;  
+}
+
 template Multiplier3 () {  
 
    // Declaration of signals.  
    signal input a;  
    signal input b;
    signal input c;
-   signal output d;  
+   signal output d; 
+   component m1 = Multiplier2();
+   component m2 = Multiplier2();
 
    // Constraints.  
-   d <== a * b * c;  
+   // d <== a * b * c;  
+   m1.a <== a;
+   m1.b <== b;
+   m2.a <== m1.c;
+   m2.b <== c;
+   d <== m2.c;
 }
 
 component main = Multiplier3();
--- Q2/scripts/compile-Multiplier3-plonk.sh
+++ Q2/scripts/compile-Multiplier3-plonk.sh
@@ -1,3 +1,40 @@
 #!/bin/bash
 
 # [assignment] create your own bash script to compile Multipler3.circom using PLONK below
+
+#!/bin/bash
+
+# [assignment] create your own bash script to compile Multipler3.circom modeling after compile-HelloWorld.sh below
+
+#!/bin/bash
+
+cd contracts/circuits
+
+mkdir _plonk
+
+if [ -f ./powersOfTau28_hez_final_10.ptau ]; then
+    echo "powersOfTau28_hez_final_10.ptau already exists. Skipping."
+else
+    echo 'Downloading powersOfTau28_hez_final_10.ptau'
+    wget https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_10.ptau
+fi
+
+echo "Compiling Multiplier3.circom..."
+
+# compile circuit
+
+circom Multiplier3.circom --r1cs --wasm --sym -o _plonk
+# Print info on the circuit
+snarkjs r1cs info _plonk/Multiplier3.r1cs
+
+# Start a new zkey and make a contribution
+
+snarkjs plonk setup _plonk/Multiplier3.r1cs powersOfTau28_hez_final_10.ptau _plonk/circuit_final.zkey
+# snarkjs zkey contribute _plonk/circuit_0000.zkey _plonk/circuit_final.zkey --name="1st Contributor Name" -v -e="random text"
+snarkjs zkey verify _plonk/Multiplier3.r1cs powersOfTau28_hez_final_10.ptau _plonk/circuit_final.zkey
+snarkjs zkey export verificationkey _plonk/circuit_final.zkey _plonk/verification_key.json
+
+# generate solidity contract
+snarkjs zkey export solidityverifier Multiplier3/circuit_final.zkey ../Multiplier3Verifier.sol
+
+cd ../..
